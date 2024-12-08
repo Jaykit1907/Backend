bbd9a0d769adc4cac3d5c97f390ce1ae4614be31
jaykit <jaykitmaurya19@gmail.com>
2024-12-08 14:44:07 +0530

    added

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get("/",(req,res)=>{
     res.send("how are you");
 })
 
+app.get("/user",(req,res)=>{
+    res.json({
+        "name":"jaykit",
+        "phone":256944555
+    });
+})
+
 app.listen(4000,()=>{
     console.log(`server running on 4000`);
 })
