195da6ec0b353d39e1aaff67d0a16324a387068c
jaykit <jaykitmaurya19@gmail.com>
2024-12-15 11:19:34 +0530

    new added

--- index.js
+++ index.js
@@ -46,58 +46,8 @@ app.get("/",(req,res)=>{
     
 });
 
-// app.post("/sendmail",(req,res)=>{
 
 
-//      mailfun(msg);
-//      console.log(req.body.to +req.body.subject+ req.body.message)
-
-//         transport.sendMail(mailoption,(err,data)=>{
-//         if(err){
-//             console.log("sending error");
-//             console.log("error:"+err);
-//         }
-//         else{
-           
-//             console.log("successfuly send mail");
-//         }
-//     })
-
-  
-    
-// });
-
-
-// app.post("/insert",async (req,res)=>{
-
-   
-//     console.log(req.body);
-//     const msg=req.body;
-
-//     mailfun(msg);
-
-//     const newUser=new User({
-//        fname:req.body.fname,
-//        lname:req.body.lname,
-//        phone:req.body.phone
-//     });
-
-//     // await newUser.save().then(()=>{console.log("data saved successsfully....."); }).catch((err)=>{console.log("error saving data")});
-//     await newUser.save().then(()=>{
-//         transport.sendMail(mailoption,(err,data)=>{
-//             if(err){
-//                 console.log("sending error");
-//                 console.log("error:"+err);
-//             }
-//             else{
-               
-//                 console.log("successfuly send mail");
-//             }
-//         })
-//         console.log("data saved successfullyy...")}).catch((err)=>{console.log("error"+err)});
-//         res.send(req.body);
-  
-// });
 
 app.post("/insert", async (req, res) => {
     console.log(req.body);
@@ -110,11 +60,11 @@ app.post("/insert", async (req, res) => {
         from: "jaykitmaurya1907@gmail.com",
         to: "jaykitmaurya19@gmail.com",
         subject: "MongoDB Data",
-        text: `New user added:\n\n${jsonData}`, // Include JSON data in the email body
+        text: `New user added:\n\n${jsonData}`, 
     };
 
     // Create a new user document
-    const newUser = new User({
+        const newUser = await new User({
         fname: req.body.fname,
         lname: req.body.lname,
         phone: req.body.phone,
@@ -123,10 +73,10 @@ app.post("/insert", async (req, res) => {
     try {
         await newUser.save(); // Save user to MongoDB
         console.log("Data saved successfully");
-        res.json({"message":"successsfully send"});
+            // res.json({"message":"successsfully send"});
 
         // Send the email
-            transport.sendMail(mailOptions, (err, data) => {
+          await transport.sendMail(mailOptions, (err, data) => {
             if (err) {
                 console.error("Error sending email:", err);
                 return res.status(500).send("User saved, but email failed to send");
